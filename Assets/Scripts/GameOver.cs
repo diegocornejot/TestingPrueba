f72bc82658af4cc51d9fcc475591cad034d1a504
Edson123789 <50849345+Edson123789@users.noreply.github.com>
2021-07-06 21:00:49 -0500

    .

--- Assets/Scripts/GameOver.cs
+++ Assets/Scripts/GameOver.cs
@@ -18,6 +18,6 @@ public class GameOver : MonoBehaviour
     }
     public void LoadGame()
     {
-        SceneManager.LoadScene("SampleScene");
+        SceneManager.LoadScene("Menu PRincipa");
     }
 }
